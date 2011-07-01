f4b66d08c52880d1b5102d0cf1a1dae6fd528ad8
Daniel L. Wang <danielw@slac.stanford.edu>
2011-07-01 11:08:15 -0700

    Use original HDF order for coordinates.

--- src/loadOperator.cc
+++ src/loadOperator.cc
@@ -35,8 +35,6 @@ void loadHdf(std::string const& filePath,
     scidb::DBArray array(aid);
     std::cout << "Added array to catalog and contructed dbarray." << std::endl;
     
-
-
     // Only handle single-attribute right now.
     int chunkMode = 0; // chunk mode (dense/sparse)
     chunkMode |=  scidb::ChunkIterator::NO_EMPTY_CHECK;
@@ -47,16 +45,23 @@ void loadHdf(std::string const& filePath,
     std::cout << "end: " << ha.end() << std::endl;
     for(H5Array::SlabIter i = ha.begin();
         i != ha.end(); ++i) {
-        // FIXME: need to fix chunkPos and coord: what do they need to be?
+        scidb::Coordinates hc(*i);
+        scidb::Coordinates c(hc.size());
+        std::reverse_copy(hc.begin(), hc.end(), c.begin());
+        // Not sure about coordinate order. HDF: minor precedes major.
         std::cout << i << std::endl;
         //ci = ai->newChunk(*i).getIterator(chunkMode);
-        scidb::Chunk& outChunk = ai->newChunk(*i);
+        scidb::Chunk& outChunk = ai->newChunk(hc);
         outChunk.allocate(i.byteSize());
         outChunk.setSparse(false); // Never sparse
+        scidb::Chunk* outChunkPtr = &outChunk;
+        // std::cout << "writing to buffer at " 
+        //           << (void*) outChunk.getData() << std::endl;
         i.readInto(outChunk.getData());
         outChunk.setCount(0);
         outChunk.write();
     }
     // Fill results
+
     //res.setString("SomeArray"); // Fill in result: name of new array
 }
